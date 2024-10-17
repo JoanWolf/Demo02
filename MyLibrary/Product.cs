ad029b0e0cb590fece8a2c960af113f40041752d
JoanWolf <dasstian@hotmail.com>
2024-10-17 17:29:21 -0500

    usando Body espresion en funciones set y get

--- MyLibrary/Product.cs
+++ MyLibrary/Product.cs
@@ -3,12 +3,29 @@
 public class Product(int id, string name, decimal unitPrice, int unitInStock)
 {
 	//Propiedades
-	public int Id { get; } = id;
-	public string Name { get;} = name;
-	public decimal UnitPrice { get;} = unitPrice;
-	public int UnitInStock { get; } = unitInStock;
+	public int Id  => id;
+	public string Name => name;
+	public decimal UnitPrice => unitPrice;
+	public int UnitInStock => unitInStock;
+	public bool Discontinued {  get; private set; }
 
+	public void SetDiscontinued()
+	{
+		Discontinued = true;
+	}
+	public bool GetStatus()
+	{
+		return Discontinued;
+	}
 }
+
+//public class Product2(int id, string name,
+//	bool discontinued) : Product(id, name, default, default)
+//{
+//	public bool Discontinued { get; set; } = discontinued;
+
+//}
+
 
 
 
