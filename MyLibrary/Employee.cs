34af2b497bae4054149ac6f0c74d1015b27d8643
JoanWolf <dasstian@hotmail.com>
2024-10-17 18:23:37 -0500

    Implementando clase abstracta  Employee

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+	public abstract class Employee
+	{
+		public int Id {  get; set; }
+		public string Name { get; set; }
+
+		public abstract decimal CalculateMonthlyPayment();
+		
+	}
+
+	public class Secretary : Employee
+	{
+		public override decimal CalculateMonthlyPayment() =>  1500;
+		
+	}
+
+	public class Manager : Employee
+	{
+
+	}
+}
