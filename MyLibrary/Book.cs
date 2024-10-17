8ffccde28db5057c558d29ffdd45dd8202827600
JoanWolf <dasstian@hotmail.com>
2024-10-17 17:45:43 -0500

    Creación clases Book y Book  de forma inmutable y haciendo uso de la herencia con la clase base Product

---
+++ MyLibrary/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLibrary
+{
+	public class Book :Product
+	{
+        public string Title { get; }
+        public Book(int id, string name, decimal unitPrice, int unitInStock, string title):
+			base (id, name, unitPrice, unitInStock)
+		{
+			Title = title;
+		}
+	}
+
+	public class Book2(int id, string name, decimal unitPrice, int unitInStock, string title) 
+		: Product(id, name, unitPrice, unitInStock)
+	{
+		public string Title => title;
+		
+	}
+}
