676987486990f4ca330924a0d57c37eb6c5f98c2
JoanWolf <dasstian@hotmail.com>
2024-10-17 17:10:04 -0500

    Haciendo uso del constructor primario

--- MyLibrary/Product.cs
+++ MyLibrary/Product.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MyLibrary;
 
-namespace MyLibrary;
+public class Product(int id, string name, decimal unitPrice, int unitInStock)
+{
+	//Propiedades
+	public int Id { get; } = id;
+	public string Name { get;} = name;
+	public decimal UnitPrice { get;} = unitPrice;
+	public int UnitInStock { get; } = unitInStock;
 
-	public class Product
-	{
-    //Propiedades
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public decimal UnitPrice { get; set; }
-		public int UnitInStock { get; set; }
-	}
-
-	//Constructor
+}
 
 
 
