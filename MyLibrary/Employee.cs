d2e10e383a61515e7b65d122a69f5063ff8341d0
JoanWolf <dasstian@hotmail.com>
2024-10-17 19:39:55 -0500

    impleemtnacion de funciones virtuales para implementar y tambien sobre escribir

--- MyLibrary/Employee.cs
+++ MyLibrary/Employee.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyLibrary
+﻿namespace MyLibrary
 {
 	public abstract class Employee
 	{
-		public int Id {  get; set; }
+		public int Id { get; set; }
 		public string Name { get; set; }
 
-		public abstract decimal CalculateMonthlyPayment();
-		
+		//public int GetInt()
+		//{
+		//	return 5;
+		//}
+		public virtual decimal CalculateMonthlyPayment()
+		{
+			return 12000;
+		}
+
+		public  int GetPaymentDays()
+		{
+			return 30;
+		}
+
 	}
 
 	public class Secretary : Employee
 	{
-		public override decimal CalculateMonthlyPayment() =>  1500;
-		
+		public override decimal CalculateMonthlyPayment() => 15000;
+
 	}
 
 	public class Manager : Employee
 	{
+		public override decimal CalculateMonthlyPayment() => 30000;
 
 	}
+
+	public class Developer : Employee
+	{
+		public  int GetPaymentDays() => 20;
+	}
+
 }
